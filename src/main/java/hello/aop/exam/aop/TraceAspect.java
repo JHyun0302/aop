5e084dcc8d3a30099960b0f114a49b7017f07864
jaehyun <wogustkfkd11@gmail.com>
2023-02-14 14:38:34 +0900

    @Trace 있으면 로그를 남김

---
+++ src/main/java/hello/aop/exam/aop/TraceAspect.java
@@ -0,0 +1,19 @@
+package hello.aop.exam.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+/**
+ * JoinPoint joinPoint: 실제 호출 대상(target) 호출
+ */
+@Slf4j
+@Aspect
+public class TraceAspect {
+    @Before("@annotation(hello.aop.exam.annotation.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
