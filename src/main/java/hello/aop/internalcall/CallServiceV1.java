fd86c80f52809e9be59e0af75e0a32dbc01ed109
jaehyun <wogustkfkd11@gmail.com>
2023-02-14 14:37:08 +0900

    대안1: 자기 자신을 의존관계 주입 받으면 해결!

---
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,30 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 대안1: 자기 자신을 의존관계 주입 받으면 해결!
+ */
+@Slf4j
+@Component
+public class CallServiceV1 {
+    private CallServiceV1 callServiceV1;
+
+    //생성자 주입시 순환 사이클 문제 발생! setter 주입!
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal(); //외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
