1f1d0b3c7b670656fad8db22223b9e178d663395
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-24 07:22:45 +0900

    :bulb: [Comment] @Around 이외 다양한 포인트 컷

--- src/main/java/hello/aop/order/aop/AspectV6Advice.java
+++ src/main/java/hello/aop/order/aop/AspectV6Advice.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.annotation.*;
 
 /**
  * @Around: joinPoint.proceed(); 필수!!
+ * 어드바이스의 첫 번째 파라미터는 ProceedingJoinPoint를 사용해야 한다.
+ * proceed() 통해 대상을 실행한다. (여러번 실행 가능)
  * @Before, @AfterReturning, @AfterThrowing, @After; 나머지는 없어도 됨!
  */
 
@@ -33,10 +35,20 @@ public class AspectV6Advice {
         }
     }
 
+    /**
+     * @Before() joinPoint.proceed(); 실행 직전까지 로직만 있으면 된다.
+     * 그 뒤는 알아서 실행해줌
+     */
+
     @Before("hello.aop.order.aop.Pointcuts.orderAndService()")
     public void doBefore(JoinPoint joinPoint) {
         log.info("[before] {}", joinPoint.getSignature());
     }
+
+    /**
+     * @AfterReturning returning = "result"와 파라미터의 Object result가 매칭되서 return값 찍을 수 있음.
+     * 하지만 result값을 return 하기 전에 조작하는 것 불가능.
+     */
 
     @AfterReturning(value = "hello.aop.order.aop.Pointcuts.orderAndService()", returning = "result")
     //returning: return되는 값 이름(파라미터와 이름 매칭)
@@ -44,12 +56,20 @@ public class AspectV6Advice {
         log.info("[return] {} return={}", joinPoint.getSignature(), result);
     }
 
+    /**
+     * @AfterThrowing throwing = "ex"와 파라미터의 Exception ex가 매칭되서 log 찍고 throw 가능
+     */
+
     @AfterThrowing(value = "hello.aop.order.aop.Pointcuts.orderAndService()", throwing = "ex")
     //throwing: 파라미터와 이름 매칭
     public void doThrowing(JoinPoint joinPoint, Exception ex) {
         log.info("[ex] {} message={}", joinPoint.getSignature(), ex.getMessage());
     }
 
+    /**
+     * @After 메서드 실행이 종료되면 실행된다.(finally와 비슷)
+     * 정상 및 예외 반환 조건을 모두 처리한다.
+     */
     @After(value = "hello.aop.order.aop.Pointcuts.orderAndService()")
     public void doAfter(JoinPoint joinPoint) {
         log.info("[after] {}", joinPoint.getSignature());
