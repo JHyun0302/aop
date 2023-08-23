d8bcfef3aee022e3ccdeaac4abc88a6d415adc71
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-24 07:24:16 +0900

    :bulb: [Comment] 프록시 내부 호출 시 aop 적용 X

--- src/test/java/hello/aop/internalcall/CallServiceV0Test.java
+++ src/test/java/hello/aop/internalcall/CallServiceV0Test.java
@@ -12,11 +12,19 @@ class CallServiceV0Test {
     @Autowired
     CallServiceV0 callServiceV0;
 
+    /**
+     * internal()을 호출 시 aop가 적용되지 않음. (프록시를 거치지 않아서 advise 호출 안됨!)
+     * 이유: AOP 프록시 external() 호출 -> 실제 external() 호출 -> this.internal() 인해 실제 internal() 호출
+     */
     @Test
     void external() {
         callServiceV0.external();
     }
 
+
+    /**
+     * external() 거치지 않고 internal() 호출 시 aop 적용됨
+     */
     @Test
     void internal() {
         callServiceV0.internal();
