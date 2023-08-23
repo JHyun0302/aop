6b5d2516a3f324839c98aed0365978fa511605b9
JHyun0302 <wogustkfkd11@gmail.com>
2023-08-24 07:34:37 +0900

    :bulb: [Comment] JDK 동적 프록시, CGLIB 프록시 차이점

--- src/test/java/hello/aop/proxyvs/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.Import;
  * JDK 동적 프록시
  */
 @Slf4j
-//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시, DI 예외 발생
-//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 프록시, 성공
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시, DI 예외 발생(JDK 동적 프록시는 인터페이스로 프록시 만들어서 Impl 모름)
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 프록시, 성공(CGLIB는 구체클래스로 프록시 만들어서 Impl 이든 인터페이스든 모두 DI 가능)
 @SpringBootTest
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
